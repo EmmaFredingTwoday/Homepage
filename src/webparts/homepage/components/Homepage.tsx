7f26fd08e4ee61d6fa6a8da8af8104856735719b
Taylor Melnik <Tamel@4c-na.local>
2023-10-10 14:02:20 -0400

    Refreshing and styling

--- src/webparts/homepage/components/Homepage.tsx
+++ src/webparts/homepage/components/Homepage.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react';
-//import styles from './Homepage.module.scss';
 import type { IHomepageProps } from './IHomepageProps';
 import { Logger, LogLevel } from "@pnp/logging";
-import { Caching } from "@pnp/queryable";
-import { SPFI, spfi } from "@pnp/sp";
+import { SPFI } from "@pnp/sp";
 import { IListItem, IListState, IResponseItem } from './interfaces'
 import { getSP } from '../pnpjsConfig';
-import { PrimaryButton  } from '@fluentui/react';
-import  PostDialog from './PostDialog'
+import { PrimaryButton } from '@fluentui/react';
+import PostDialog from './PostDialog'
 import dayjs from 'dayjs';
-import styles from './Homepage.module.scss'
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
+
 
 export default class Homepage extends React.Component<IHomepageProps, IListState> {
-
    
   private LOG_SOURCE = "🅿PnPjsExample";
   private _sp: SPFI;  
@@ -30,54 +29,74 @@ export default class Homepage extends React.Component<IHomepageProps, IListState
     this._sp = getSP();
   }
 
-
   public render(): React.ReactElement<IHomepageProps> {
     const {
-      listName,
+      // listName,
       company,
       userDisplayName,
       intranetUrl,
       publicSite
     } = this.props;
 
-    console.log("intranetUrl " +intranetUrl + " listname "+ listName );
+    const articleStyle = {
+      padding: '15px',
+      borderBottom: '1px solid #e5e5e5'
+    };
+
+    const h1Style = {
+      fontSize: '24px',
+      margin: '10px 0'
+    };
+
+    const pStyle = {
+      margin: '10px 0',
+      lineHeight: '1.5'
+    };
 
     let shortTime = "";
 
-    if(company === userDisplayName){
-    return (
-      <section> 
-      <div className={styles.buttonMargin}>       
-        <span className={styles.someSpace}><PrimaryButton text="Skapa inlägg" title="Skapa inlägg" onClick={this._createPost} /> </span>
-        <span className={publicSite? undefined : styles.hidden }><PrimaryButton text="Intranät" title="Intranät" href={intranetUrl} /></span>     
-      </div>  
-        {this.state.items.map((item: any) => (
-            shortTime = dayjs(item.Created).format("HH:mm"),
-            <article>              
-              <h1>{item.Title}</h1>               
-              <p className={styles.content}>{item.Content}</p>
-              <span>{shortTime}</span> <span>{item.Author0}</span>                       
-            </article>
-          ))}             
-      </section>
-    )} else {
+    if(company === userDisplayName) {
       return (
-        <section> 
+        <section style={{ fontFamily: 'Arial, sans-serif' }}>
+          <div style={{ textAlign: 'center', margin: '10px 0 30px' }}>
+            <span style={{ margin: '0 15px' }}>
+              <PrimaryButton text="Skapa inlägg" title="Skapa inlägg" onClick={this._createPost} />
+            </span>
+            {publicSite && <PrimaryButton text="Intranät" title="Intranät" href={intranetUrl} />}
+          </div>  
+
           {this.state.items.map((item: any) => (
-              shortTime = dayjs(item.Created).format("HH:mm"),
-              <article>              
-                <h1>{item.Title}</h1>               
-                <p className={styles.content}>{item.Content}</p>
+            shortTime = dayjs(item.Created).fromNow(),
+            <article style={articleStyle}>              
+              <h1 style={h1Style}>{item.Title}</h1>
+              <p style={pStyle}>{item.Content}</p>
+              <span>{shortTime}</span> <span>{item.Author0}</span>
+            </article>
+          ))}
+        </section>
+      )
+    } else {
+      return (
+        <section style={{ fontFamily: 'Arial, sans-serif' }}>
+          {this.state.items.map((item: any) => (
+              shortTime = dayjs(item.Created).fromNow(),
+              <article style={articleStyle}>              
+                <h1 style={h1Style}>{item.Title}</h1>               
+                <p style={pStyle}>{item.Content}</p>
                 <span>{shortTime}</span> <span>{item.Author0}</span>                       
               </article>
-            ))}             
+            ))}
         </section>
-    )}
+      )
+    }
   }
 
   private _createPost = async (): Promise<void> => {
     const taskDialog = new PostDialog(      
-      async (header, content, author) => {},
+      async (header, content, author) => {
+        // Once the post has been made, fetch the latest list items.
+        await this._readAllListItems();
+      },
       async () => alert('You closed the dialog!'),
       this.props.listName
     );
@@ -87,16 +106,12 @@ export default class Homepage extends React.Component<IHomepageProps, IListState
 
   private _readAllListItems = async(): Promise<void> => {
     try{
-      const spCache = spfi(this._sp).using(Caching({store:"session"}));
-        console.log("I _readall")
-
-      const response: IListItem[] = await spCache.web.lists
+      const response: IListItem[] = await this._sp.web.lists
         .getByTitle(this.props.listName)
         .items
         .select("Id", "Title", "Content", "Author0", "Created")
         .orderBy("Created", false)();
 
-      // use map to convert IResponseItem[] into our internal object IFile[]
       const items: IListItem[] = response.map((item: IResponseItem) => {
         return {
           Id: item.Id,
@@ -104,11 +119,10 @@ export default class Homepage extends React.Component<IHomepageProps, IListState
           Content: item.Content,
           Author0: item.Author0,
           Created: item.Created,
-          header:""
+          header: ""
         };
       });
       this.setState({items});
-      console.log("items: " +items);
     }
     catch(err){
       Logger.write(`${this.LOG_SOURCE} (_readAllKwitterItems) - ${JSON.stringify(err)} - `, LogLevel.Error);
